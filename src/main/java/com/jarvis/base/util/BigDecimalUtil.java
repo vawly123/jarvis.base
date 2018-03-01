b3350821e555f51e5b389a81b413cb0cc8f03638
ping <vawly_pqy@163.com>
2018-03-01 11:35:03 +0800

    添加BigDecimal工具类1

--- src/main/java/com/jarvis/base/util/BigDecimalUtil.java
+++ src/main/java/com/jarvis/base/util/BigDecimalUtil.java
@@ -6,6 +6,7 @@ public class BigDecimalUtil {
     /**
      * 由于Java的简单类型不能够精确的对浮点数进行运算，这个工具类提供精
      * 确的浮点数运算，包括加减乘除和四舍五入。
+     * .
      */
 //默认除法运算精度
     private static final int DEF_DIV_SCALE = 10;
